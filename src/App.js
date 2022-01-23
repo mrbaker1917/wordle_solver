5e4d94e6c8f1cd0c2d554e0ce653f92def2b8dbd
mrbaker1917 <mrbaker1917@gmail.com>
2022-01-23 15:36:16 -0800

    Starts style for main page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,6 +6,20 @@ function App() {
       <header className="App-header">
         <h1>WORDLE SOLVER</h1>
       </header>
+      <body className='App-body'>
+      <p>If you want some help to solve wordle, just follow the instructions below.</p>
+      <ul>
+        <li>Tip 1: Enter the word "AROSE" as your first guess.</li>
+        <li>Tip 2: If any letters were green after that first guess, enter them in the correct place below:</li>
+        <li><ul className='place-list'>
+          <li className='placers'><input/></li>
+          <li className='placers'><input/></li>
+          <li className='placers'><input/></li>
+          <li className='placers'><input/></li>
+          <li className='placers'><input/></li>
+          </ul></li>
+      </ul>
+    </body>
     </div>
   );
 }
