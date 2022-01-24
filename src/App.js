44e9f6833d080ca8d2b79564e7c7bf864d649f84
Mark R Baker <mrbaker1917@gmail.com>
2022-01-23 18:38:04 -0800

    adds rest of input fields

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -6,20 +6,49 @@ function App() {
       <header className="App-header">
         <h1>WORDLE SOLVER</h1>
       </header>
-      <body className='App-body'>
-      <p>If you want some help to solve wordle, just follow the instructions below.</p>
-      <ul>
-        <li>Tip 1: Enter the word "AROSE" as your first guess.</li>
-        <li>Tip 2: If any letters were green after that first guess, enter them in the correct place below:</li>
-        <li><ul className='place-list'>
-          <li className='placers'><input/></li>
-          <li className='placers'><input/></li>
-          <li className='placers'><input/></li>
-          <li className='placers'><input/></li>
-          <li className='placers'><input/></li>
-          </ul></li>
-      </ul>
-    </body>
+      <body className="App-body">
+        <p>
+          If you want some help to solve wordle, just follow the instructions
+          below.
+        </p>
+        <form>
+          <ul>
+            <li>Tip 1: Enter the word "AROSE" as your first guess.</li>
+            <li>
+              Tip 2: If any letters were green after that first guess, enter
+              them in the correct place below:
+            </li>
+            <li>
+              <ul className="place-list">
+                <li className="placers">
+                  <input />
+                </li>
+                <li className="placers">
+                  <input />
+                </li>
+                <li className="placers">
+                  <input />
+                </li>
+                <li className="placers">
+                  <input />
+                </li>
+                <li className="placers">
+                  <input />
+                </li>
+              </ul>
+            </li>
+            <li>Tip 3: Input in the space below any letters that are grey in your guess:
+              <br/>
+              <input className="banned-letters" />
+            </li>
+            <li>Tip 4: Input in the field below any letters that were correct, but in the wrong place:
+              <br/>
+              <input className="right-letters" />
+            </li>
+          </ul>
+          <button className="submit-button">Submit</button>
+        </form>
+      </body>
     </div>
   );
 }
