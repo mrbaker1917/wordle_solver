12fcfddffa0a77e0eb0109d7d1b1c0b197a4968e
Mark R Baker <mrbaker1917@gmail.com>
2022-02-23 18:26:35 -0800

    adds attribution to mrbaker1917

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>WORDLE SOLVER</h1>
+        <a href="https://mrbaker1917.com" target="_blank" rel="noreferrer" title="Click to go to my website!">Created by mrbaker1917</a>
       </header>
       <div className="App-body">
         <p>
