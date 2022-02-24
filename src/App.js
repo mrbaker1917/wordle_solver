7e16be977f9f6a7bd8700542f7a00d4aaf8c5244
Mark R Baker <mrbaker1917@gmail.com>
2022-02-23 19:24:46 -0800

    adds hover pink to title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>WORDLE SOLVER</h1>
+        <h1><span>W</span><span>O</span><span>R</span><span>D</span><span>L</span><span>E</span><span>  </span> <span>S</span><span>O</span><span>L</span><span>V</span><span>E</span><span>R</span></h1>
         <a href="https://mrbaker1917.com" target="_blank" rel="noreferrer" title="Click to go to my website!">Created by mrbaker1917</a>
       </header>
       <div className="App-body">
