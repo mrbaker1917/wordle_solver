0cdee2cd726c639c04e0cba5f20a03a389ffb0c2
Mark R. Baker <mrbaker1917@gmail.com>
2023-04-07 14:48:55 -0700

    fixes "_blank" sec issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1><span>W</span><span>o</span><span>r</span><span>d</span><span>l</span><span>e</span><span>  </span><span>S</span><span>o</span><span>l</span><span>v</span><span>e</span><span>r</span></h1>
-        <a href="https://mrbaker1917.com" target="_blank" rel="noreferrer" title="Click to go to my website!">Created by mrbaker1917</a>
+        <a href="https://mrbaker1917.com" target="_blank" rel="noopener noreferrer" title="Click to go to my website!">Created by mrbaker1917</a>
       </header>
       <div className="App-body">
         <p>
