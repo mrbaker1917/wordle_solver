a016b0296dc61e9ad88ad36db29db9fc164d21cf
mrbaker1917 <mrbaker1917@gmail.com>
2026-05-13 05:28:30 -0700

    updates next to 16.2.6

--- .next/server/pages/_error.js
+++ .next/server/pages/_error.js
@@ -1,8 +1,8 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
-R.c("server/chunks/ssr/node_modules_next_55786baf._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__dac0e50d._.js")
-R.c("server/chunks/ssr/[externals]_react_jsx-runtime_7ad7b9a3._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__2574aea5._.js")
-R.c("server/chunks/ssr/pages__app_43455d5d.js")
-R.m(910)
-module.exports=R.m(910).exports
+R.c("server/chunks/ssr/node_modules_next_dist_04ek7i_._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0-nak4m._.js")
+R.c("server/chunks/ssr/[externals]_react_jsx-runtime_0l46tkh._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0pbg048._.js")
+R.c("server/chunks/ssr/pages__app_0bmbb.b.js")
+R.m(3530)
+module.exports=R.m(3530).exports
