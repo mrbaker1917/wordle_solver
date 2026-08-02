78549717dd0cb16e751b023ff6a1153428438c3a
Mark R. Baker <mrbaker1917@gmail.com>
2026-08-02 15:44:04 -0700

    Updated dependencies to secure, current versions and verified the app still builds successfully.

--- .next/server/pages/_error.js
+++ .next/server/pages/_error.js
@@ -1,8 +1,8 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
-R.c("server/chunks/ssr/node_modules_next_dist_07phak1._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1s2rhzg._.js")
+R.c("server/chunks/ssr/0c64_next_dist_16x7usr._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__01dlkfh._.js")
 R.c("server/chunks/ssr/[externals]_react_jsx-runtime_0-3ytx0._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__16h8hpd._.js")
-R.c("server/chunks/ssr/pages__app_0kc3ow8.js")
-R.m(3530)
-module.exports=R.m(3530).exports
+R.c("server/chunks/ssr/[root-of-the-server]__0vu604i._.js")
+R.c("server/chunks/ssr/OneDrive_Documents_GitHub_wordle_solver_pages__app_0zi4m08.js")
+R.m(6872)
+module.exports=R.m(6872).exports
